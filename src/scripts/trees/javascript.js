d3fbcd1b4e91b596566b2d4d8227f61551040e71
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-29 09:10:48 +0000

    Deploy jekku/mg-tree to github.com/jekku/mg-tree.git:gh-pages

---
+++ src/scripts/trees/javascript.js
@@ -0,0 +1,164 @@
+const react = [
+  {
+    id: 'state',
+    title: 'State Management',
+    tooltip: {
+      content: 'Effective use of hooks and proper state shape. Knowledge of when and when not to use state'
+    },
+    children: [],
+    icon: './icons/react-original.svg'
+  },
+  {
+    id: 'components',
+    title: 'Component Organization',
+    tooltip: {
+      content: 'Effective organization of components by business context. Knowledge of HOC, dependency injectible components, and testable components'
+    },
+    children: [],
+    icon: './icons/react-original.svg'
+  }
+]
+
+const es6 = [
+  {
+    id: 'reactjs',
+    title: 'React JS',
+    tooltip: {
+      content: 'Ability to build reusable components and make proper state management'
+    },
+    children: react,
+    icon: './icons/react-original.svg'
+  },
+  {
+    id: 'async-constructs',
+    title: 'Async',
+    tooltip: {
+      content: 'The power to write out-branching javascript in different ways'
+    },
+    children: [
+      {
+        id: 'promises',
+        title: 'Promises',
+        tooltip: {
+          content: 'Writing promises safely'
+        },
+        children: [],
+        icon: './icons/javascript-original.svg'
+      },
+      {
+        id: 'generators',
+        title: 'Generators',
+        tooltip: {
+          content: 'Understanding star funs and yield'
+        },
+        children: [],
+        icon: './icons/javascript-original.svg'
+      },
+      {
+        id: 'async-await',
+        title: 'Async-Await',
+        tooltip: {
+          content: 'Reduction of mental gymnastics when necessary'
+        },
+        children: [],
+        icon: './icons/javascript-original.svg'
+      }
+    ],
+    icon: './icons/javascript-original.svg'
+  },
+  {
+    id: 'typescript',
+    title: 'TypeScript',
+    tooltip: {
+      content: 'JS Dialect reinforced with types. ES6 derivative.'
+    },
+    children: [],
+    icon: './icons/typescript-original.svg'
+  }
+]
+
+const constructs = [
+  {
+    id: 'dom-operations',
+    title: 'DOM Operations',
+    tooltip: {
+      content: 'The ability to query and manipulate DOM elements'
+    },
+    children: [],
+    icon: './icons/javascript-original.svg'
+  },
+  {
+    id: 'events',
+    title: 'Events',
+    tooltip: {
+      content: 'Understanding how JS events work and how to use them'
+    },
+    children: [],
+    icon: './icons/javascript-original.svg'
+  },
+  {
+    id: 'devtools',
+    title: 'Devtools',
+    tooltip: {
+      content: 'The ability to leverage browser devtools for easier debugging and building'
+    },
+    children: [],
+    icon: './icons/javascript-original.svg'
+  }
+]
+
+const node = [
+  {
+    id: 'webpack',
+    title: 'Webpack',
+    tooltip: {
+      content: 'Harness the ability to create fast asset and code pipelines with minimal configuration'
+    },
+    icon: './icons/webpack-original.svg',
+    children: []
+  }
+]
+
+const js = [
+  {
+    id: 'constructs',
+    title: 'The Browser',
+    tooltip: {
+      content: 'Understanding of vanilla JS browser constructs',
+    },
+    children: constructs,
+    icon: './icons/chrome-original.svg'
+  },
+  {
+    id: 'es6',
+    title: 'EcmaScript 6',
+    tooltip: {
+      content:  'The capability to write modern javascript with ease',
+    },
+    children: es6,
+    icon: './icons/javascript-original.svg'
+  },
+  {
+    id: 'nodejs',
+    title: 'Node JS basics',
+    tooltip: {
+      content: 'A basic understanding of Node JS runtime built-ins and usage'
+    },
+    children: node,
+    icon: './icons/nodejs-original.svg'
+  }
+]
+
+const data = [
+  {
+    id: 'javascript',
+    title: 'Javascript',
+    tooltip: {
+      content: 'Mashup Garage\'s core frontend logic and build toolchain',
+    },
+    children: js,
+    icon: './icons/javascript-original.svg'
+  }
+]
+
+export default data
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = {
+  entry: './src/scripts/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      {from: './src/index.html', to: './index.html'},
+      {from: './src/icons/', to: './icons'}
+    ]),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  },
+  watchOptions: {
+    aggregateTimeout: 300,
+    poll: 1000
+  }
+}
